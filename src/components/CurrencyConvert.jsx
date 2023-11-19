4bfc86bf742d41aee912a4c788e128b0f967c0bc
faeltwister <faeltwister@gmail.com>
2023-11-19 20:27:53 -0300

    css

--- src/components/CurrencyConvert.jsx
+++ src/components/CurrencyConvert.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import './currency.css'
 const CurrencyConvert = () => {
   return (
-    <div>CurrencyConvert</div>
+    <div className='converter'>
+        <h3>Conversor de moedas</h3>
+        <input type="number" />
+        <span>Selecione as moedas</span>
+        <select >
+            <option value="BRL">BRL</option>
+        </select>
+        <span>Para</span>
+        <select >
+            <option value="USD">USD</option>
+        </select>
+        <h4>BRL - USD</h4>
+        <p>20 Reais valem 5 Dólares</p>
+
+    </div>
   )
 }
 
